21ac8f67853c5b31710ddd0fda739ba064de7bbf
Fredrik Wollsén <fredrik@neam.se>
2015-02-23 14:15:52 +0100

    Item types helper methods: all(), choiceFormatLabels(), modelSlugs()

--- helper/yii/helper.php
+++ helper/yii/helper.php
@@ -13,5 +13,46 @@ echo "<?php\n";
  */
 class <?= $helperName . "\n" ?>
 {
-    // TODO: Implement this template
+
+    static public function all()
+    {
+        return [
+<?php foreach ($itemTypes as $itemType): ?>
+            '<?= $itemType->model_class; ?>' => '<?= $itemType->table; ?>',
+<?php endforeach; ?>
+        ];
+    }
+
+    static public function label($modelClass, $n)
+    {
+
+    }
+
+    /**
+     * Frontend UI labels for the models
+     * @return array
+     */
+    static public function choiceFormatLabels()
+    {
+        return [
+<?php foreach ($itemTypes as $itemType): ?>
+            '<?= $itemType->model_class; ?>' => '<?= $itemType->title_choice_format; ?>',
+<?php endforeach; ?>
+        ];
+    }
+
+    /**
+     * Slugs for the model classes
+     * Used when setting semantic routes
+     * @return array
+     */
+    static public function modelSlugs()
+    {
+        return [
+<?php foreach ($itemTypes as $itemType): ?>
+            '<?= $itemType->model_class; ?>' => '<?= $itemType->slug; ?>',
+<?php endforeach; ?>
+        ];
+    }
+
 }
