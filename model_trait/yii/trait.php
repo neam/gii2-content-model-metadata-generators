2663f5080bffbfcdfb4ef4ffa60f7ee27c2efed5
Fredrik Wollsén <fredrik@neam.se>
2015-02-24 01:53:52 +0100

    Labels, hints and workflow-related metadata in trait template

--- model_trait/yii/trait.php
+++ model_trait/yii/trait.php
@@ -14,4 +14,75 @@ echo "<?php\n";
 trait <?= $traitName . "\n" ?>
 {
     // TODO: Implement this template
+    /**
+     * Define status-dependent fields
+     * @return array
+     */
+    public function statusRequirements()
+    {
+        return array(
+<?php foreach ($statusRequirements as $statusRequirementRef => $statusRequirement): ?>
+            '<?= "$statusRequirementRef" ?>' => array(
+    <?php foreach ($statusRequirement as $attribute): ?>
+                <?= "'$attribute',\n" ?>
+    <?php endforeach; ?>
+            ),
+<?php endforeach; ?>
+        );
+    }
+
+    /**
+     * Define step-dependent fields
+     * @return array
+     */
+    public function flowSteps()
+    {
+        return array(
+<?php foreach ($flowSteps as $flowStepRef => $flowStep): ?>
+            '<?= "$flowStepRef" ?>' => array(
+    <?php foreach ($flowStep as $attribute): ?>
+                <?= "'$attribute',\n" ?>
+    <?php endforeach; ?>
+            ),
+<?php endforeach; ?>
+        );
+    }
+
+    public function flowStepCaptions()
+    {
+        return array(
+<?php foreach ($flowStepCaptions as $name=>$flowStepCaption): ?>
+            <?= "'$name' => " . $generator->generateString($flowStepCaption) . ",\n" ?>
+<?php endforeach; ?>
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return array_merge(
+            parent::attributeLabels(), array(
+<?php foreach ($labels as $name => $label): ?>
+            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+<?php endforeach; ?>
+            )
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeHints()
+    {
+        return array_merge(
+            parent::attributeHints(), array(
+<?php foreach ($hints as $name => $label): ?>
+            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+<?php endforeach; ?>
+            )
+        );
+    }
+
 }
